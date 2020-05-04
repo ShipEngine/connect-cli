e577fe9fbda9d9f6acff5fa868fcea89b2472b56
Pierce Harmon <pierceh.dev@gmail.com>
2020-05-04 15:04:01 -0500

    W.I.P.

--- src/api-client.ts
+++ src/api-client.ts
@@ -2,7 +2,7 @@
 
 export default class APIClient {
   /**
-   * Here we can do things related to networking but that should not be coupled in the actual API implementation
+   * Here we can do things related to networking but that should not be coupled in the actual API client implementation
    * - set the header for which version of the CLI is calling the API
    * - check to see if the user is logged in w/ the API key
    */
