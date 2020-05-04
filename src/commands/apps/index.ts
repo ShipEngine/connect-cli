d14b404aa816bdb54e042ab46f0955cd5891f36e
Pierce Harmon <pierceh.dev@gmail.com>
2020-05-04 12:59:19 -0500

    W.I.P.

---
+++ src/commands/apps/index.ts
@@ -0,0 +1,17 @@
+import Command from "../command";
+import { flags } from "@oclif/command";
+
+export default class AppsIndex extends Command {
+  static description =
+    "create a new project to develop a custom ShipEngine integration";
+
+  static flags = {
+    help: flags.help({ char: "h" }),
+  };
+
+  static examples = ["apps:new"];
+
+  async run() {
+    this._help();
+  }
+}
