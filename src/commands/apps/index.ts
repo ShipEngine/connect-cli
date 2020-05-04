8aa90246a09c7cf490c3d493e6de725a11b7fe40
Pierce Harmon <pierceh.dev@gmail.com>
2020-05-04 13:40:32 -0500

    W.I.P.

--- src/commands/apps/index.ts
+++ src/commands/apps/index.ts
@@ -1,4 +1,4 @@
-import Command from "../command";
+import Command from "../../command";
 import { flags } from "@oclif/command";
 
 export default class AppsIndex extends Command {
--- src/commands/apps/new.ts
+++ src/commands/apps/new.ts
@@ -1,4 +1,4 @@
-import Command from "../command";
+import Command from "../../command";
 import { flags } from "@oclif/command";
 import { createTemplate } from "../../shipengine-core/create-template";
 
