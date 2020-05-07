0621c6728552e54b4886b60e2ffeaa722d6536b3
Pierce Harmon <pierceh.dev@gmail.com>
2020-05-07 11:58:49 -0500

    wip

--- src/commands/apps/new.ts
+++ src/commands/apps/new.ts
@@ -1,6 +1,7 @@
 import BaseCommand from "../../base-command";
 import { flags } from "@oclif/command";
-import { createTemplate } from "../../shipengine-core/create-template";
+import { createEnv } from "yeoman-environment";
+// import { createTemplate } from "../../shipengine-core/create-template";
 
 export default class New extends BaseCommand {
   static description =
@@ -10,9 +11,31 @@ export default class New extends BaseCommand {
     help: flags.help({ char: "h" }),
   };
 
-  static examples = ["$ shipengine apps:test"];
+  static examples = ["$ shipengine apps:new"];
 
   async run() {
-    createTemplate();
+    const { flags, args } = this.parse(New);
+    // const options = flags.options ? flags.options.split(",") : [];
+
+    await this.generate({
+      // type: this.type,
+      // path: args.path,
+      // options,
+      // defaults: flags.defaults,
+      // force: flags.force,
+    });
+  }
+
+  private async generate(generatorOptions: object = {}) {
+    const env = createEnv();
+
+    env.register(require.resolve("../../generators/apps-new"), "apps:new");
+
+    await new Promise((resolve, reject) => {
+      env.run("apps:new", generatorOptions, (err: Error, results: any) => {
+        if (err) reject(err);
+        else resolve(results);
+      });
+    });
   }
 }
