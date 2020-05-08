2729b0955349e8f90b6880715118e4660fd3c37d
Pierce Harmon <pierceh.dev@gmail.com>
2020-05-08 12:24:43 -0500

    W.I.P.

--- test/commands/apps/new.spec.js
+++ test/commands/apps/new.spec.js
@@ -1,32 +1,29 @@
-// "use strict";
+"use strict";
 
-// const { expect, test } = require("@oclif/test");
-// const sinon = require("sinon");
-// const yeomanEnv = require("yeoman-environment");
+const { expect, test } = require("@oclif/test");
+const sinon = require("sinon");
+const yeomanEnv = require("yeoman-environment");
 
-// const yeomanEnvMock = sinon.mock({
-//   run: (namespace, options, callback) => {
-//     callback();
-//   },
-//   register: () => {},
-// });
+const double = {
+  run: (namespace, options, callback) => {
+    callback();
+  },
+  register: () => {},
+};
 
-// describe("apps:new", () => {
-//   beforeEach(() => {
-//     sinon.stub(yeomanEnv, "createEnv").returns(yeomanEnvMock);
-//   });
+describe("apps:new", () => {
+  beforeEach(() => {
+    sinon.stub(yeomanEnv, "createEnv").returns(double);
+  });
 
-//   test
-//     .stdout()
-//     .command(["apps:new"])
-//     .it("calls the apps:new generator with the given args and flags", (ctx) => {
-//       expect(ctx.stdout).to.contain("Time to build a ShipEngine app!\n");
-//       yeomanEnvMock.expects("register").once();
-//       yeomanEnvMock.expects("run").once();
-//       yeomanEnvMock.verify();
-//     });
+  test
+    .stdout()
+    .command(["apps:new"])
+    .it("calls the apps:new generator with the given args and flags", (ctx) => {
+      expect(ctx.stdout).to.contain("Time to build a ShipEngine app!\n");
+    });
 
-//   afterEach(() => {
-//     sinon.restore();
-//   });
-// });
+  afterEach(() => {
+    sinon.restore();
+  });
+});
