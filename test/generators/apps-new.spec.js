0519447e931bba4452f75a9ed8cb61c87d05b318
Pierce Harmon <pierceh.dev@gmail.com>
2020-05-08 08:22:42 -0500

    W.I.P.

--- test/generators/apps-new.spec.js
+++ test/generators/apps-new.spec.js
@@ -31,40 +31,114 @@ describe("generator apps:new", function () {
             "package.json",
             "tsconfig.json",
           ]);
+
+          assert.jsonFileContent("package.json", { main: "lib/index.js" });
         });
     });
   });
 
-  // describe("when configured without test", function () {
-  //   it("generates a new app", function () {
-  //     // The object returned acts like a promise, so return it to wait until the process is done
-  //     return helpers
-  //       .run(AppsNew, {
-  //         resolved: path.join(__dirname, "../../lib/generators/apps-new"),
-  //         namespace: "apps:new",
-  //       })
-  //       .withPrompts({ coffee: false })
-  //       .withPrompts({ coffee: false })
-  //       .withPrompts({ coffee: false })
-  //       .withPrompts({ coffee: false })
-  //       .withArguments(["test-app"]) // Mock the arguments
-  //       .then(function () {
-  //         assert.file([
-  //           "src/index.ts",
-  //           "test/index.test.ts",
-  //           "test/mocha.opts",
-  //           "test/tsconfig.json",
-  //           ".editorconfig",
-  //           ".eslintignore",
-  //           ".eslintrc",
-  //           "LICENSE",
-  //           "README.md",
-  //           "README.md",
-  //           "package.json",
-  //           "tsconfig.json",
-  //         ]);
-  //         assert.noFile(["test/index.test.ts", "mocha.opts"]);
-  //       });
-  //   });
-  // });
+  describe("when configured without test", function () {
+    it("generates a new app", function () {
+      // The object returned acts like a promise, so return it to wait until the process is done
+      return helpers
+        .run(AppsNew, {
+          resolved: path.join(__dirname, "../../lib/generators/apps-new"),
+          namespace: "apps:new",
+        })
+        .withPrompts({ eslint: true })
+        .withPrompts({ mocha: false })
+        .withPrompts({ pkg: true })
+        .withPrompts({ typescript: true })
+        .withArguments(["test-app"]) // Mock the arguments
+        .then(function () {
+          assert.file([
+            "src/index.ts",
+            ".editorconfig",
+            ".eslintignore",
+            ".eslintrc",
+            "LICENSE",
+            "README.md",
+            "package.json",
+            "tsconfig.json",
+          ]);
+
+          assert.noFile([
+            "test/index.test.ts",
+            "test/mocha.opts",
+            "test/tsconfig.json",
+          ]);
+
+          assert.jsonFileContent("package.json", { main: "lib/index.js" });
+        });
+    });
+  });
+
+  describe("when configured without eslint", function () {
+    it("generates a new app", function () {
+      // The object returned acts like a promise, so return it to wait until the process is done
+      return helpers
+        .run(AppsNew, {
+          resolved: path.join(__dirname, "../../lib/generators/apps-new"),
+          namespace: "apps:new",
+        })
+        .withPrompts({ eslint: false })
+        .withPrompts({ mocha: true })
+        .withPrompts({ pkg: true })
+        .withPrompts({ typescript: true })
+        .withArguments(["test-app"]) // Mock the arguments
+        .then(function () {
+          assert.file([
+            "src/index.ts",
+            "test/index.test.ts",
+            "test/mocha.opts",
+            "test/tsconfig.json",
+            ".editorconfig",
+            "LICENSE",
+            "README.md",
+            "package.json",
+            "tsconfig.json",
+          ]);
+
+          assert.noFile([".eslintignore", ".eslintrc"]);
+          assert.jsonFileContent("package.json", { main: "lib/index.js" });
+        });
+    });
+  });
+
+  describe("when configured without TypeScript", function () {
+    it("generates a new app", function () {
+      // The object returned acts like a promise, so return it to wait until the process is done
+      return helpers
+        .run(AppsNew, {
+          resolved: path.join(__dirname, "../../lib/generators/apps-new"),
+          namespace: "apps:new",
+        })
+        .withPrompts({ eslint: true })
+        .withPrompts({ mocha: true })
+        .withPrompts({ pkg: true })
+        .withPrompts({ typescript: false })
+        .withArguments(["test-app"]) // Mock the arguments
+        .then(function () {
+          assert.file([
+            "src/index.js",
+            "test/index.test.js",
+            "test/mocha.opts",
+            ".editorconfig",
+            ".eslintrc",
+            "LICENSE",
+            "README.md",
+            "package.json",
+          ]);
+
+          assert.noFile([
+            "src/index.ts",
+            "test/tsconfig.json",
+            ".eslintignore",
+            "tsconfig.json",
+          ]);
+
+          assert.jsonFileContent("package.json", { main: "src/index.js" });
+        });
+    });
+  });
 });
