946a387cbed63cec71ba435c8df618de7778c1e0
Pierce Harmon <pierceh.dev@gmail.com>
2020-05-08 09:17:10 -0500

    wip

--- test/commands/apps/new.spec.js
+++ test/commands/apps/new.spec.js
@@ -1,44 +1,27 @@
-// "use strict";
+"use strict";
 
-// const { expect, test } = require("@oclif/test");
-// const fs = require("fs");
-// const path = require("path");
-// const testDirectory = "test-app";
-// const pathToTestDirectory = path.join(__dirname, `../../../${testDirectory}`);
+const { expect, test } = require("@oclif/test");
+const sinon = require("sinon");
+const yeomanEnv = require("yeoman-environment");
 
-// async function deleteTestApp() {
-//   return new Promise(function (resolve, reject) {
-//     fs.access(pathToTestDirectory, (directoryNotFoundError) => {
-//       if (!directoryNotFoundError) {
-//         fs.rmdir(
-//           pathToTestDirectory,
-//           {
-//             recursive: true,
-//           },
-//           (error) => {
-//             if (error) {
-//               reject(`could not delete ${testDirectory}`);
-//             } else {
-//               resolve("done");
-//             }
-//           },
-//         );
-//       } else {
-//         resolve("done");
-//       }
-//     });
-//   });
-// }
+describe("apps:new", () => {
+  before(() => {
+    sinon.stub(yeomanEnv, "createEnv").returns({
+      run: (namespace, options, callback) => {
+        callback();
+      },
+      register: () => {},
+    });
+  });
 
-// describe("apps:new", () => {
-//   // afterEach(async () => {
-//   //   await deleteTestApp();
-//   // });
+  test
+    .stdout()
+    .command(["apps:new"])
+    .it("scaffolds a new app", (ctx) => {
+      expect(ctx.stdout).to.contain("Time to build a ShipEngine app!\n");
+    });
 
-//   test
-//     .stdout()
-//     .command(["apps:new", testDirectory, "-y"])
-//     .it("scaffolds a new app", (ctx) => {
-//       expect(ctx.stdout).to.contain("Time to build a ShipEngine app!\n");
-//     });
-// });
+  after(() => {
+    sinon.restore();
+  });
+});
