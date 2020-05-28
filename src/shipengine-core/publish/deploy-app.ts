3085d26ecbf907d75afce2c9ed583f18e7fbf5ae
Randall Krauskopf <randall.krauskopf@gmail.com>
2020-05-28 07:06:08 -0500

    remove carrier id, currently no longer needed in the publish process

--- src/shipengine-core/publish/deploy-app.ts
+++ src/shipengine-core/publish/deploy-app.ts
@@ -30,7 +30,6 @@ export async function deployApp(packageTarballlName: string, apiClient: APIClien
   let form = new FormData();
   
   form.append("deployment", fs.createReadStream(tarPath));
-  form.append("id", app.id );
   form.append("name", app.manifest.name );
   form.append("type", app.type);
   form.append("version", app.manifest.version);
