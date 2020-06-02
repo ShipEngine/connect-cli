28440ae642a3f9d8fa4fe6186d124559781c499c
Pierce Harmon <pierceh.dev@gmail.com>
2020-06-02 15:19:08 -0500

    W.I.P.

--- src/shipengine-core/test-app/tiny-test/suite.ts
+++ src/shipengine-core/test-app/tiny-test/suite.ts
@@ -3,6 +3,9 @@ import { App } from "../../utils/types";
 import { TransactionPOJO } from "@shipengine/integration-platform-sdk";
 import { v4 } from "uuid";
 
+interface Config {
+  sessionMock?: object;
+}
 export default abstract class Suite {
   app: App;
   protected transactionWithMockSession: TransactionPOJO;
@@ -12,11 +15,23 @@ export default abstract class Suite {
   constructor(app: App) {
     this.app = app;
 
+    let shipengineConfig: Config;
+
+    try {
+      shipengineConfig = require(`${process.cwd()}/shipengine.config.js`);
+    } catch {
+      shipengineConfig = {};
+    }
+
+    const sessionMock = shipengineConfig.sessionMock
+      ? shipengineConfig.sessionMock
+      : {};
+
     this.transactionWithMockSession = {
       id: v4(),
       isRetry: false,
       useSandbox: false,
-      session: {},
+      session: sessionMock,
     };
 
     this._testCache = this.tests();
