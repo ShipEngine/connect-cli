b4ecae355dffc296c7ef006ba574aa76a2551254
Randall Krauskopf <randall.krauskopf@gmail.com>
2020-06-01 19:13:20 -0500

    add additional tests

--- test/specs/commands/apps/publish.spec.js
+++ test/specs/commands/apps/publish.spec.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const { expect, test } = require("@oclif/test");
-const nock = require("nock");
 const path = require("path");
+const cli = require("cli-ux");
 
 let cwd;
-describe.only("apps:publish", () => {
+let projectPath;
+describe("apps:publish", () => {
 
   test
     .stdout()
@@ -29,18 +30,42 @@ describe.only("apps:publish", () => {
     })
     .do(() => {
       cwd = process.cwd();
-      process.chdir(path.join(cwd, "test", "fixtures", "apps", "carrier", "valid"));
+      projectPath = path.join(cwd, "test", "fixtures", "apps", "carrier", "valid");
+      process.chdir(projectPath);
     })
-    // .finally(() => {
-    //   process.chdir(cwd);
-    // })
-    .stdout()
+    .stderr()
     .command(["apps:publish"])
     // .exit(0)
     .do(() => {
       process.chdir(cwd);
     })
-    .it("calls the apps:publish command", (ctx) => {
-      expect(ctx.stdout).to.contain("To track the status of the deployment");
+    .it("should publish the app", (ctx) => {
+      // It should also delete the npm package tarball
+      expect(projectPath).to.not.include.files(["carrier-inline-app-0.0.1.tgz"]);
+      expect(ctx.stderr).to.contain("Publishing App");
+    });
+
+  test
+    .stub(cli.default, "prompt", () => {
+      return () => "12345";
+    })
+    .nock("http://localhost:3000", (api) => {
+      api.post(uri => uri.endsWith("deploys")).reply(200, { deployId: "12345" });
+    })
+    .nock("http://localhost:3000", (api) => {
+      api.get(uri => uri.includes("deploys/")).reply(200, { status: "running" });
+    })
+    .do(() => {
+      cwd = process.cwd();
+      projectPath = path.join(cwd, "test", "fixtures", "apps", "carrier", "valid");
+      process.chdir(projectPath);
+    })
+    .stderr()
+    .command(["apps:publish", "--watch"])
+    .do(() => {
+      process.chdir(cwd);
+    })
+    .it("should publish the app and watch the deployment status", (ctx) => {
+      expect(ctx.stderr).to.contain("Your app was published successfully");
     });
 });
