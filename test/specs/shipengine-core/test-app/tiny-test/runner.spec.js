1a5ab75850bf1b085eef5bd7f7a3de92298753a5
Pierce Harmon <pierceh.dev@gmail.com>
2020-06-03 14:57:54 -0500

    W.I.P.

---
+++ test/specs/shipengine-core/test-app/tiny-test/runner.spec.js
@@ -0,0 +1,16 @@
+// "use strict";
+
+// const { expect } = require("chai");
+// const {
+//   Runner,
+// } = require("../../../../../lib/shipengine-core/test-app/tiny-test/runner");
+
+// describe("Runner", () => {
+//   describe("testCache setter", () => {
+//     it("overwrites the existing tests", () => {
+//       expect(subject.toString()).to.be.a("string");
+//       expect(subject.toString()).to.be.include(subject.title);
+//       expect(subject.toString()).to.be.include(subject.truncatedSha());
+//     });
+//   });
+// });
