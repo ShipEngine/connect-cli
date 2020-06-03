21bdcade337333e4baf775cf653422c194c6ef9d
Pierce Harmon <pierceh.dev@gmail.com>
2020-06-03 11:29:25 -0500

    W.I.P.

---
+++ test/specs/shipengine-core/utils/read-file.spec.js
@@ -0,0 +1,33 @@
+"use strict";
+const { expect } = require("chai");
+const subject = require("../../../../lib/shipengine-core/utils/read-file");
+
+describe("readFile", () => {
+  // it("reads and imports a js file as a module", async () => {
+  //   const response = await subject.readFile(
+  //     "test/fixtures/files/read-javascript-test.js",
+  //   );
+  //   expect(response.foo).equal("bar");
+  // });
+
+  it("reads and imports a json file as a module", async () => {
+    const response = await subject.readFile(
+      "test/fixtures/files/read-json-test.json",
+    );
+    expect(response.foo).equal("bar");
+  });
+
+  it("reads and imports a yaml file as a module", async () => {
+    const response = await subject.readFile(
+      "test/fixtures/files/read-yaml-test.yaml",
+    );
+    expect(response.foo).equal("bar");
+  });
+
+  it("reads and imports a yml file as a module", async () => {
+    const response = await subject.readFile(
+      "test/fixtures/files/read-yaml-test.yml",
+    );
+    expect(response.foo).equal("bar");
+  });
+});
