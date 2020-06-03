7360186de7ee606eefae9d066f09dab4db7be479
Pierce Harmon <pierceh.dev@gmail.com>
2020-06-03 10:28:40 -0500

    W.I.P.

--- src/shipengine-core/utils/read-file.ts
+++ src/shipengine-core/utils/read-file.ts
@@ -6,7 +6,6 @@ import { promises as fs } from "fs";
 import * as jsYaml from "js-yaml";
 import * as json5 from "json5";
 import * as path from "path";
-import * as tsNode from "ts-node";
 import { error as sdkError } from "@shipengine/integration-platform-sdk/lib/internal";
 
 /**
@@ -79,46 +78,6 @@ async function importJavaScriptModule<T>(filePath: string): Promise<T> {
   }
 }
 
-// Ensures TS-Node is only registered once
-let tsNodeRegistered = false;
-
-/**
- * Returns the default export of the specified TypeScript module
- */
-async function importTypeScriptModule<T>(filePath: string): Promise<T> {
-  if (!tsNodeRegistered) {
-    tsNodeRegistered = true;
-
-    tsNode.register({
-      // Don't do full type checking. Just transpile TS to JS.
-      transpileOnly: true,
-
-      // Don't search for a tsconfig.json. Just use the compilerOptions specified below.
-      skipProject: true,
-
-      // TypeScript compiler options
-      compilerOptions: {
-        // These options ensure compatibility with Node 10
-        moduleResolution: "Node",
-        module: "CommonJS",
-        target: "ES2019",
-
-        // Allow import JSON files
-        resolveJsonModule: true,
-
-        // Enable interoperability between CJS and ESM
-        esModuleInterop: true,
-
-        // Enable sourcemaps, so errors map to original source
-        sourceMap: true,
-      },
-    });
-  }
-
-  // Once TS-Node is registered, TypeScript files can be imported just like JavaScript files
-  return importJavaScriptModule(filePath);
-}
-
 /**
  * Reads a file based on its file extension
  */
@@ -129,16 +88,10 @@ export async function readFile<T>(filePath: string): Promise<T> {
       return readYamlFile(filePath);
 
     case ".json":
-    case ".jsonc":
-    case ".json5":
       return readJsonFile(filePath);
 
     case ".js":
-    case ".mjs":
       return importJavaScriptModule(filePath);
-
-    case ".ts":
-      return importTypeScriptModule(filePath);
 
     default:
       return (readTextFile(filePath) as unknown) as T;
