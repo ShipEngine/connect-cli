2e1e2e215a0543e8496336723c8fc7103273d02f
Pierce Harmon <pierceh.dev@gmail.com>
2020-09-09 11:17:53 -0500

    W.I.P.

--- src/commands/versions.ts
+++ src/commands/versions.ts
@@ -6,6 +6,20 @@ export default class Versions extends BaseCommand {
   public static hidden = true;
 
   async run(): Promise<void> {
-    return Object.entries(getVersions()).forEach(([key, value]: [string, string]) => this.log(`${key} ${value}`))
+    const versions = getVersions()
+    let longestKeyVal = 0;
+
+    Object.keys(versions).forEach((key: string) => {
+      if (key.length > longestKeyVal) {
+        longestKeyVal = key.length
+      }
+    });
+
+    return Object.entries(versions).forEach(([key, value]: [string, string]) => this.log(`${key}${this.addPadding(longestKeyVal, key)} ${value}`))
+  }
+
+  private addPadding(longestKeyVal: number, currentKey: string) {
+    const padding = longestKeyVal - currentKey.length;
+    return " ".repeat(padding);
   }
 }
