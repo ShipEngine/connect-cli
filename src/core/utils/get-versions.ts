3e088235ff9aa5e65d1fb5fb42d9dc9d948c68c1
Pierce Harmon <pierceh.dev@gmail.com>
2020-09-09 10:41:02 -0500

    W.I.P.

--- src/core/utils/get-versions.ts
+++ src/core/utils/get-versions.ts
@@ -1,5 +1,3 @@
-/* eslint-disable unicorn/no-abusive-eslint-disable */
-
 interface PJson {
   name: string;
   version: string;
@@ -10,19 +8,14 @@ interface PJson {
 export default function getVersions(): Record<string, string> {
   // We have to use `require()` here instad of `import`
   // because the "package.json" file is outside of the "src" directory.
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
   const cliPjson = require("../../../package.json") as PJson;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
   const sdkPjson = require("@shipengine/connect-sdk/package.json") as PJson;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
   const loaderPjson = require("@shipengine/connect-loader/package.json") as PJson;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
   const localDevApiPjson = require("@shipengine/connect-local-dev-api/package.json") as PJson;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
   const localDevUiPjson = require("@shipengine/connect-local-dev-ui/package.json") as PJson;
 
   return {
@@ -31,6 +24,6 @@ export default function getVersions(): Record<string, string> {
     "@shipengine/connect-sdk": sdkPjson.version,
     "@shipengine/connect-loader": loaderPjson.version,
     "@shipengine/connect-local-dev-api": localDevApiPjson.version,
-    "@shipengine/connect-local-dev-ur": localDevUiPjson.version
+    "@shipengine/connect-local-dev-ui": localDevUiPjson.version
   }
 }
